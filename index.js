fbf6745d33ba8ccaa0645e3c35d7e86888ffcf5c
joneseyit <joneseyit@protonmail.com>
2018-10-12 04:45:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+// function nestedTarget(){
+//   const nestedList = document.querySelector('#ndiv div div div );
+ 
+// return  nestedList;
+// }
+
+function increaseRankBy(n){
+  const currentRank = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  
+  console.log(currentRank.length, 'length of ranks');
+  for(let i = 0; i < currentRank.length; i++){
+    console.log(currentRank[i].innerHTML, 'rankHTML');
+    console.log(currentRank[i],'this is I plus stuff')
+  currentRank[i].innerHTML = n + parseInt(currentRank[i].innerHTML, 10);
+    console.log(currentRank[i].innerHTML, 'rankHTML');
+  }
+}
+
+function deepestChild(){
+  const grandNode = document.getElementById('grand-node').querySelectorAll('div');
+  
+}
