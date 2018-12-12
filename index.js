e488200a8eafa767cc405e07b40dd5f54c7bcff8
Andrew Jones <joneseyit@protonmail.com>
2018-12-12 15:00:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector("#nested .target")
+}
+
+function deepestChild(){
+  const x = document.getElementById('grand-node')
+  const y = x.querySelectorAll("div")
+  return y[y.length -1]
+}
+
+function increaseRankBy(x){
+  debugger
+  const items = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < items.length; i++){
+    debugger
+    items[i].innerHTML = (parseInt(items[i].innerHTML) + x).toString();
+  }
+  return items
+}
